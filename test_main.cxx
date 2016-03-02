8cb910511b6e428b281af02bbefe50686534433f
Arcoth <columbo@gmx-topmail.de>
2016-03-02 12:27:12 +0000

    Fixed formatting

--- test_main.cxx
+++ test_main.cxx
@@ -2,11 +2,11 @@
 
 STRING_ENUM
 (
-    class MyEnum : unsigned long long, MyEnumInfo,
+	class MyEnum : unsigned long long, MyEnumInfo,
 
-    (Enumerator1, 7),
-    (Enumerator2, 89),
-    (Enumerator3)
+	(Enumerator1, 7),
+	(Enumerator2, 89),
+	(Enumerator3)
 )
 
 #include <iostream>
@@ -15,6 +15,6 @@ int main()
 {
 	constexpr auto s1 = MyEnumInfo::toString( MyEnumInfo::fromString("Enumerator1").first ),
 	               s2 = MyEnumInfo::toString( MyEnumInfo::fromString("Enumerator2").first );
-    constexpr auto i = MyEnumInfo::fromString("Enumerator3").first;
+	constexpr auto i = MyEnumInfo::fromString("Enumerator3").first;
 	std::cout << s1 << '\n' << s2 << '\n' << (int)i;
 }
